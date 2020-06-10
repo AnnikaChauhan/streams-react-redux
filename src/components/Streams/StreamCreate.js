ab59f15b8f95fb01f1ec03c690c73e0d2f42730a
Annika Chauhan <annikac0711@gmail.com>
2020-06-10 12:27:19 +0100

    error handling added to the form with some additional semantic UI classes added

--- src/components/Streams/StreamCreate.js
+++ src/components/Streams/StreamCreate.js
@@ -3,23 +3,37 @@ import { Field, reduxForm } from 'redux-form';
 
 
 class StreamCreate extends React.Component {
-    renderInput({ input, label }) {
+    renderError({error, touched}){
+        if(touched && error){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+
+    //this returned an error of 'undefined' when it was NOT an arrow function so had to be changed to an arrow function, the issue is because it's in a react/js class with context inside it - when renderInput is called it is called with an unknown value of 'this' - discussion of this issue is in video 242
+    renderInput = ({ input, label, meta }) => {
+        // console.log(meta);
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
-                <input {...input} placeholder={label} />
+                <input {...input} placeholder={label} autoComplete="off" />
+                {this.renderError(meta)}
             </div>
         );
     };
 
-    onSubmit(formValues){
+    onSubmit(formValues) {
         //this is run after the redux form runs it's on submit function which includes the prevent default, this secondary function is for YOU (the dev) to add any additional code that you want to occur after the redux submit
         console.log(formValues);
     };
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button className="ui button primary">Submit</button>
@@ -28,6 +42,20 @@ class StreamCreate extends React.Component {
     };
 };
 
+const validate = (formValues) => {
+    //every time the user interacts with a form the validation is called and validates all of the form inputs
+    const errors = {};
+    if (!formValues.title) {
+        // only runs if the user didn't input a title value
+        errors.title = 'You must enter a title';
+    }
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+    return errors;
+}
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate: validate
 })(StreamCreate);
