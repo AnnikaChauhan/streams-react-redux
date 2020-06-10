6039eaa5dd1b72e7bc0a9877e6b70fb9dca9c711
Annika Chauhan <annikac0711@gmail.com>
2020-06-10 11:58:54 +0100

    config for redux dev tools added, package for redux form also added, additional info added to reducer root file added to handle form in state management and form component added to stream create

--- src/components/Streams/StreamCreate.js
+++ src/components/Streams/StreamCreate.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 
-const StreamCreate = () => {
-    return ( 
-        <div>StreamCreate</div>
-     );
-}
- 
-export default StreamCreate;
+class StreamCreate extends React.Component {
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} placeholder={label} />
+            </div>
+        );
+    };
+
+    onSubmit(formValues){
+        //this is run after the redux form runs it's on submit function which includes the prevent default, this secondary function is for YOU (the dev) to add any additional code that you want to occur after the redux submit
+        console.log(formValues);
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    };
+};
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+//the 'as formReducer' part is an optional rename as the name reducer is a bit inconvenient
+import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
